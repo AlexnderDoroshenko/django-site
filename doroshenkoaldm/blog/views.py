2e3be4d47865c748fe8933012a6408a203edcdde
AlexnderDoroshenko <doroshenkoaldm@gmail.com>
2019-11-30 22:35:36 +0200

    create vcs for site

---
+++ doroshenkoaldm/blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return HttpResponse('Blog under construction')
+def test(request):
+    return HttpResponse("Test branch")
